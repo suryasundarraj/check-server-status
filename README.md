151f5758c365506016dd2f6c10e92f7612709d9d
suryasundarraj <suryasundarraj@live.com>
2023-01-31 20:29:51 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# check-server-status
+# BAT Script to get all the server status 
 
-To run the code on Windows Machine 
+Step 1: Update the servers.txt file with the servers needed to be tested (Ensure all the servers are mapped with public key)
+
+Format: 
+user_name@ip_address
+
+Sample servers.txt:
+user1@192.168.1.100
+user2@192.168.2.120
+user3@192.12.34.43
+
+Step 2: To run the code on Windows Machine 
 
 .\getServerStatus.bat
 
 or 
 
-D
+Double click on ".bat" file to execute
---
+++ getServerStatus.bat
@@ -0,0 +1,20 @@
+@echo off
+del serverStatus.log
+echo |set /p="Started..."
+echo:
+echo|set /p="Checking all Server's latest Status" > serverStatus.log
+echo: >> serverStatus.log
+echo %DATE% %TIME% >> serverStatus.log
+echo: >> serverStatus.log
+
+for /f %%i in (servers.txt) do (
+    echo |set /p="Testing %%i"
+    echo:
+    echo %%i >> serverStatus.log
+    echo: >> serverStatus.log
+    (ssh -t %%i "uptime && date") 1>>serverStatus.log 2>&1
+    echo: >> serverStatus.log
+    echo: >> serverStatus.log
+)
+
+echo|set /p="End of the Script" >> serverStatus.log
